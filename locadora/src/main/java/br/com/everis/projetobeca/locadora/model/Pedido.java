ec83d447e8017486dad80ee99129d8f93e0fde13
rafaelrspatrocinio <rafaels.patrocinio@hotmail.com>
2021-10-05 14:09:34 -0300

    Alteracao da Classe Pedido - Inclusão dos Getters e Setters

--- locadora/src/main/java/br/com/everis/projetobeca/locadora/model/Pedido.java
+++ locadora/src/main/java/br/com/everis/projetobeca/locadora/model/Pedido.java
@@ -1,6 +1,8 @@
 package br.com.everis.projetobeca.locadora.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.*;
@@ -9,18 +11,20 @@ import java.util.List;
 
 
 @Entity
-@Table(name="PEDIDOS")
+@Table(name = "PEDIDOS")
+@Getter
+@Setter
 public class Pedido {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-//    private StatusPedido status;
-//    private Funcionario funcionario;
-//    private List<Produto> listaProdutos;
+//  private StatusPedido status;
+//  private Funcionario funcionario;
+//  private List<Produto> listaProdutos;
     private Double valorTotal;
     private Double TotalPago;
-//    private Pagamento pagamento;
+//  private Pagamento pagamento;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
     private LocalDate data;
