ee09483002f68c2092b08b989e44171deae017ff
rafaelrspatrocinio <rafaels.patrocinio@hotmail.com>
2021-10-05 14:29:32 -0300

    Alteracao da Classe Cliente - Criacao da classe Cliente

--- locadora/src/main/java/br/com/everis/projetobeca/locadora/model/Cliente.java
+++ locadora/src/main/java/br/com/everis/projetobeca/locadora/model/Cliente.java
@@ -1,11 +1,20 @@
 package br.com.everis.projetobeca.locadora.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.*;
 
+@Entity
+@Table(name = "CLIENTES")
+@Getter
+@Setter
+public class Cliente extends Pessoa{
 
-public class Cliente {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String login;
+    private String senha;
 }
