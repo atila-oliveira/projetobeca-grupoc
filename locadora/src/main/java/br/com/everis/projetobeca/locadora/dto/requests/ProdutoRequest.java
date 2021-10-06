084388519fc2873b4b891a05d6d360ee978b9aa3
atila-oliveira <atilateioli@gmail.com>
2021-10-06 16:04:29 -0300

    reorganizando?

--- locadora/src/main/java/br/com/everis/projetobeca/locadora/dto/requests/ProdutoRequest.java
+++
@@ -1,4 +0,0 @@
-package br.com.everis.projetobeca.locadora.dto.requests;
-
-public class ProdutoRequest {
-}
--- locadora/src/main/java/br/com/everis/projetobeca/locadora/dto/responses/ProdutosResponse.java
+++
@@ -1,9 +0,0 @@
-package br.com.everis.projetobeca.locadora.dto.responses;
-
-public class ProdutosResponse {
-
-    private Long id;
-
-    private String nome;
-    private String sobrenome;
-}
