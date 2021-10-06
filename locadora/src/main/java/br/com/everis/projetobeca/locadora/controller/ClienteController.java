603967524e8cc115b8ac5c45e861d505c4843863
rafaelrspatrocinio <rafaels.patrocinio@hotmail.com>
2021-10-06 16:03:43 -0300

    Anotação @Repository

--- locadora/src/main/java/br/com/everis/projetobeca/locadora/controller/ClienteController.java
+++ locadora/src/main/java/br/com/everis/projetobeca/locadora/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.everis.projetobeca.locadora.controller;
 import br.com.everis.projetobeca.locadora.model.Cliente;
 import br.com.everis.projetobeca.locadora.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,15 +12,14 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/clientes")
+@Controller
 public class ClienteController {
 
     @Autowired
     ClienteService clienteService;
 
-    @GetMapping
-    public ModelAndView getClient(){
+    @RequestMapping(value = "/clientes", method = RequestMethod.GET)
+    public ModelAndView getPost(){
         ModelAndView mv = new ModelAndView("clientes");
         List<Cliente> clientes = clienteService.findAll();
         mv.addObject("clientes", clientes);
