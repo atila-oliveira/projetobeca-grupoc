75fead362b3f25a47007fe4e58379625a913ef29
atila-oliveira <atilateioli@gmail.com>
2021-10-07 18:04:36 -0300

    Sufoco resolvido

--- locadora/src/main/java/br/com/everis/projetobeca/locadora/controller/ProdutoController.java
+++ locadora/src/main/java/br/com/everis/projetobeca/locadora/controller/ProdutoController.java
@@ -1,47 +1,49 @@
 package br.com.everis.projetobeca.locadora.controller;
 
-import br.com.everis.projetobeca.locadora.controller.dto.ProdutoDTO;
+import br.com.everis.projetobeca.locadora.model.Cliente;
 import br.com.everis.projetobeca.locadora.model.Produto;
+import br.com.everis.projetobeca.locadora.service.ClienteService;
 import br.com.everis.projetobeca.locadora.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@RestController
+@Controller
 public class ProdutoController {
 
     @Autowired
     ProdutoService produtoService;
 
-    @RequestMapping(value = "locadora/produtos", method = RequestMethod.GET)
-    public ModelAndView buscarProdutos(){
-        ModelAndView mv = new ModelAndView("produtos");
+    @RequestMapping(value = "/locadora/produtos", method = RequestMethod.GET)
+    public ModelAndView buscarProduto(){
+        ModelAndView mv = new ModelAndView("clientes");
         List<Produto> produtos = produtoService.findAll();
         mv.addObject("produtos", produtos);
         return mv;
     }
 
-    //@RequestMapping(value = "/locadora/produtos", method = RequestMethod.GET)
-    public String getProdutoform(){
-        return "/locadora/produtos/cadastrar";
+    @RequestMapping(value = "/novoproduto", method = RequestMethod.GET)
+    public String getProdutoForm(){
+        return "produtoForm";
     }
 
-    @RequestMapping(value = "/locadora/produtos/cadastrar", method = RequestMethod.POST)
-    public  String cadastarProduto(@Valid Produto produto, BindingResult result, RedirectAttributes atributos){
-        if(result.hasErrors()){
-            atributos.addFlashAttribute("mensagem","Favor preencher todos os dados!");
-            return "redirect:locadora/produtos/cadastrar";
+    @RequestMapping(value = "/novoproduto", method = RequestMethod.POST)
+    public String saveProduto(@Valid Produto produto, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors() ) {
+            attributes.addFlashAttribute("messagem", "Verifique de os campos obrigatórios foram preenchidos");
+            return "redirect:/novoproduto";
         }
-        produtoService.save(produto);
-        return "redirect:/locadora";
-    }
 
+        produtoService.save(produto);
+        return "redirect:/produtos";
+    }
 }
+
+
