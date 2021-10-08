9306cf3133361da4191ec172a75c93a374e5104b
rafaelrspatrocinio <rafaels.patrocinio@hotmail.com>
2021-10-08 09:39:59 -0300

    CLIENTE E FUNCIONARIO - CADASTRANDO E EXIBINDO OK

--- locadora/src/main/java/br/com/everis/projetobeca/locadora/model/Cliente.java
+++ locadora/src/main/java/br/com/everis/projetobeca/locadora/model/Cliente.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotBlank;
 public class Cliente {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(nullable = false, unique = false)
--- locadora/src/main/java/br/com/everis/projetobeca/locadora/model/Funcionario.java
+++ locadora/src/main/java/br/com/everis/projetobeca/locadora/model/Funcionario.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Funcionario  {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @OneToMany
